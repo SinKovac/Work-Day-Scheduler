8c2db88a2bdc704bbe8306600fd583c19fae0eae
SinKovac <71274524+SinKovac@users.noreply.github.com>
2020-11-04 12:15:43 -0500

    updated script

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ var workDay = {
   
 function hourNumberFromHourString(hourString) {
     switch(hourString) {
-      case "9 AM": return 8;
-      case "10 AM": return 9;
-      case "11 AM": return 10;
-      case "12 AM": return 11;
-      case "1 PM": return 12;
-      case "2 PM": return 13;
-      case "3 PM": return 14;
-      case "4 PM": return 15;
-      case "5 PM": return 16;
+      case "9 AM": return 9;
+      case "10 AM": return 10;
+      case "11 AM": return 11;
+      case "12 AM": return 12;
+      case "1 PM": return 13;
+      case "2 PM": return 14;
+      case "3 PM": return 15;
+      case "4 PM": return 16;
+      case "5 PM": return 17;
     }
   }
 
